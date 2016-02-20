d57005080cae4da5aed3c7ad3249857591127b1a
Jiaju Ni <nijiaju@hotmail.com>
2016-02-20 15:14:00 -0600

    added connection list

--- minet-netclass-w16-proj2/src/core/tcp_module.cc
+++ minet-netclass-w16-proj2/src/core/tcp_module.cc
@@ -14,6 +14,7 @@
 #include <iostream>
 
 #include "Minet.h"
+#include "tcpstate.h"
 
 
 using std::cout;
@@ -23,68 +24,74 @@ using std::string;
 
 int main(int argc, char *argv[])
 {
-    MinetHandle mux, sock;
+  MinetHandle mux, sock;
 
-    MinetInit(MINET_TCP_MODULE);
+  MinetInit(MINET_TCP_MODULE);
 
-    mux=MinetIsModuleInConfig(MINET_IP_MUX) ? MinetConnect(MINET_IP_MUX) : MINET_NOHANDLE;
-    sock=MinetIsModuleInConfig(MINET_SOCK_MODULE) ? MinetAccept(MINET_SOCK_MODULE) : MINET_NOHANDLE;
+  ConnectionList<ConnectionToStateMapping<TCPState>> clist;
 
-    if (MinetIsModuleInConfig(MINET_IP_MUX) && mux==MINET_NOHANDLE) {
-        MinetSendToMonitor(MinetMonitoringEvent("Can't connect to mux"));
-        return -1;
-    }
+  mux=MinetIsModuleInConfig(MINET_IP_MUX) ? MinetConnect(MINET_IP_MUX) : MINET_NOHANDLE;
+  sock=MinetIsModuleInConfig(MINET_SOCK_MODULE) ? MinetAccept(MINET_SOCK_MODULE) : MINET_NOHANDLE;
 
-    if (MinetIsModuleInConfig(MINET_SOCK_MODULE) && sock==MINET_NOHANDLE) {
-        MinetSendToMonitor(MinetMonitoringEvent("Can't accept from sock module"));
-        return -1;
-    }
+  if (MinetIsModuleInConfig(MINET_IP_MUX) && mux==MINET_NOHANDLE) {
+    MinetSendToMonitor(MinetMonitoringEvent("Can't connect to mux"));
+    return -1;
+  }
 
-    MinetSendToMonitor(MinetMonitoringEvent("tcp_module handling TCP traffic"));
+  if (MinetIsModuleInConfig(MINET_SOCK_MODULE) && sock==MINET_NOHANDLE) {
+    MinetSendToMonitor(MinetMonitoringEvent("Can't accept from sock module"));
+    return -1;
+  }
 
-    MinetEvent event;
+  MinetSendToMonitor(MinetMonitoringEvent("tcp_module handling TCP traffic"));
+  cerr << "tcp_module handling TCP traffic......\n";
 
-    while (MinetGetNextEvent(event)==0) {
+  MinetEvent event;
+
+  while (MinetGetNextEvent(event)==0) {
     // if we received an unexpected type of event, print error
-        if (event.eventtype!=MinetEvent::Dataflow
-            || event.direction!=MinetEvent::IN) {
-            MinetSendToMonitor(MinetMonitoringEvent("Unknown event ignored."));
+    if (event.eventtype!=MinetEvent::Dataflow || event.direction!=MinetEvent::IN) {
+      MinetSendToMonitor(MinetMonitoringEvent("Unknown event ignored."));
+      cerr << "Invalid event from Minet\n" << endl;
+    } else {
+      //  Data from the IP layer below  //
+      if (event.handle==mux) {
+        Packet p;
+        MinetReceive(mux,p);
+        unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
+        cerr << "estimated header len=" << tcphlen << "\n";
+        p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
+        IPHeader iph=p.FindHeader(Headers::IPHeader);
+        TCPHeader tcph=p.FindHeader(Headers::TCPHeader);
+	    Connection c;
 
-            // if we received a valid event from Minet, do processing
+        cerr << "IP Header is "<<iph<<"\n";
+        cerr << "TCP Header is "<<tcph << "\n";
+		if (!tcph.IsCorrectChecksum(p)) {
+			cerr << "Checksum check FAILED!!!\n";
+			//continue;
+		}
 
-            if (event.handle == mux) {
-                // fill by ni
-            }
-
-            if (event.handle == sock) {
-                // fill by shen
-            }
-
-        } else {
-            cerr << "invalid event from Minet" << endl;
-            //  Data from the IP layer below  //
-            if (event.handle==mux) {
-                Packet p;
-                MinetReceive(mux,p);
-                unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
-                cerr << "estimated header len="<<tcphlen<<"\n";
-                p.ExtractHeaderFromPayload<TCPHeader>(tcphlen);
-                IPHeader ipl=p.FindHeader(Headers::IPHeader);
-                TCPHeader tcph=p.FindHeader(Headers::TCPHeader);
-
-                cerr << "TCP Packet: IP Header is "<<ipl<<" and ";
-                cerr << "TCP Header is "<<tcph << " and ";
-
-                cerr << "Checksum is " << (tcph.IsCorrectChecksum(p) ? "VALID" : "INVALID");
-            }
-            //  Data from the Sockets layer above  //
-
-            if (event.handle==sock) {
-                SockRequestResponse s;
-                MinetReceive(sock,s);
-                cerr << "Received Socket Request:" << s << endl;
-            }
-        }
+		// Identifty the connection with 5-tuple
+        iph.GetDestIP(c.src);
+		iph.GetSourceIP(c.dest);
+		iph.GetProtocol(c.protocol);
+		tcph.GetDestPort(c.srcport);
+		tcph.GetSourcePort(c.destport);
+		ConnectionList<ConnectionToStateMapping<TCPState>>::iterator cs = clist.FindMatching(c);
+		if(cs == clist.end()) {
+		  cerr << "ERROR: invalid connection detected!!!\n";
+		} else {
+		  cerr << "INFO:  identified connection\n";
+		}
+      }
+          //  Data from the Sockets layer above  //
+      if (event.handle==sock) {
+        SockRequestResponse s;
+        MinetReceive(sock,s);
+        cerr << "Received Socket Request:" << s << endl;
+      }
     }
-    return 0;
+  }
+  return 0;
 }
